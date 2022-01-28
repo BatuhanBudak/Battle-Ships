28aea6dc92d038894cd5106f7e6baae4238a2a6f
Batuhan <batuhan.budak@gmail.com>
2022-01-29 01:21:00 +0300

    Added 3 tests for checking node status

--- tests.test.js
+++ tests.test.js
@@ -9,16 +9,35 @@ test("Can create ship on empty node", () => {
   expect(gFactory.createShipAtCoord(0, 0, 5)).toEqual(true);
 });
 
-test("Can check the coordinate that have been fired is empty", () => {
+test("Can check the coordinate that has been fired is empty correctly", () => {
   let gFactory = gameBoardFactory(5, 5);
   gFactory.createShipAtCoord(0, 0, 5);
   gFactory.receiveAttack(0, 1)
   expect(gFactory.getMissedShotCoordsArr()[0]).toHaveLength(2);
 });
-test("Can check the coordinate that have been fired is occupied", () => {
+test("Can check the coordinate that has been fired is occupied correctly", () => {
   let gFactory = gameBoardFactory(5, 5);
   gFactory.createShipAtCoord(0, 0, 5);
   expect(gFactory.receiveAttack(4, 0)).toBe(true);
+});
+test("Can check the fired coordinate\'s status correctly", () => {
+  let gFactory = gameBoardFactory(5, 5);
+  gFactory.createShipAtCoord(0, 0, 5);
+  gFactory.receiveAttack(4, 0);
+  expect(gFactory.getBoardMatrix()[4][0].getNodeStatus()).toBe('hit');
+});
+
+test("Can check the unharmed coordinate\'s status correctly", () => {
+  let gFactory = gameBoardFactory(5, 5);
+  gFactory.createShipAtCoord(0, 0, 5);
+  gFactory.receiveAttack(4, 0);
+  expect(gFactory.getBoardMatrix()[3][0].getNodeStatus()).toBe('default');
+});
+test("Can check the missed coordinate\'s status correctly", () => {
+  let gFactory = gameBoardFactory(5, 5);
+  gFactory.createShipAtCoord(0, 0, 5);
+  gFactory.receiveAttack(4, 1);
+  expect(gFactory.getBoardMatrix()[4][1].getNodeStatus()).toBe('miss');
 });
 
 test("Can check the damage status of all ships on matrix", () => {
@@ -29,7 +48,7 @@ test("Can check the damage status of all ships on matrix", () => {
   gFactory.createShipAtCoord(0, 3, 2);
   expect(gFactory.checkDamageStatusOfShips()).toBe(false);
 });
-test("Can check whether an individual ship has sunk or not", () => {
+test("Can check whether an individual ship has sunked or not", () => {
   let gFactory = gameBoardFactory(5, 5);
   gFactory.createShipAtCoord(0, 0, 5);
   gFactory.receiveAttack(0, 0);
@@ -44,8 +63,19 @@ test('Player and computer can attack each other', () => {
     gFactory.createShipAtCoord(0, 0, 5);
     let cFactory = gameBoardFactory(5, 5);
     cFactory.createShipAtCoord(0, 0, 5);
-    let player = Player(gFactory);
     let computer = ComputerPlayer(cFactory);
-    expect(player.attack(0,0,computer)).toBe(true);    
+    let player = Player(gFactory, computer);
+    expect(player.attack(0,0)).toBe(true);    
     expect(computer.attack(player)).toBeDefined();    
 });
+test('Player can sunk computer\'s ship', () => {
+  let gFactory = gameBoardFactory(5, 5);
+  gFactory.createShipAtCoord(0, 0, 5);
+  let cFactory = gameBoardFactory(5, 5);
+  cFactory.createShipAtCoord(0, 0, 1);
+  let computer = ComputerPlayer(cFactory);
+  let player = Player(gFactory, computer);
+  player.attack(0,0);
+  expect(cFactory.checkDamageStatusOfShips()).toBe(true);    
+});
+
