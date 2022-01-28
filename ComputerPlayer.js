86123ad4c6eb32c52f832898d831acc9c6d81643
Batuhan <batuhan.budak@gmail.com>
2022-01-29 01:20:21 +0300

    Removed hasPlayed prop

--- ComputerPlayer.js
+++ ComputerPlayer.js
@@ -1,35 +1,32 @@
-
 const ComputerPlayer = (board) => {
-    let computerBoard = board;
-    let hasPlayed = false;
-    let takenShotsCoordinatesArray = [];
+  let computerBoard = board;
+  let takenShotsCoordinatesArray = [];
 
-    const changePlayStatus = () => hasPlayed = !hasPlayed;
+  const randomIntFromInterval = (min, max) => {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  const generateRandomShootCoords = () => {
+    let row = randomIntFromInterval(0, 4); // This numbers will be changed after giving the matrix fixed size for setup
+    let col = randomIntFromInterval(0, 4); // This numbers will be changed after giving the matrix fixed size for setup
+    return [row, col];
+  };
 
-    const randomIntFromInterval= (min, max) => { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
-      }
-    const generateRandomShootCoords = () => {
-        let row = randomIntFromInterval(0, 4); // This numbers will be changed after giving the matrix fixed size for setup
-        let col = randomIntFromInterval(0, 4); // This numbers will be changed after giving the matrix fixed size for setup
-        return [row, col];
+  const receiveAttack = (row, col) => {
+    return computerBoard.receiveAttack(row, col);
+  };
+  const attack = (enemy) => {
+    let missedShotsArr = enemy.getBoardOfPlayer().getMissedShotCoordsArr();
+    let [row, col] = generateRandomShootCoords();
+    while (
+      missedShotsArr.includes([row, col]) ||
+      takenShotsCoordinatesArray.includes([row, col])
+    ) {
+      [row, col] = generateRandomShootCoords();
     }
-     
-    const receiveAttack = (row, col) => {
-        return computerBoard.receiveAttack(row, col);
-    }
-    const attack = (enemy) => {
-        if(!hasPlayed){
-            changePlayStatus();
-            let missedShotsArr = enemy.getBoardOfPlayer().getMissedShotCoordsArr();
-            let [row, col] = generateRandomShootCoords();
-            while(missedShotsArr.includes([row,col]) || takenShotsCoordinatesArray.includes([row, col])){
-                [row,col] = generateRandomShootCoords();
-            }
-            takenShotsCoordinatesArray.push([row, col]);
-            return enemy.receiveAttack(row, col);
-        }
-    }
-    return {attack, receiveAttack};
-}
-export {ComputerPlayer};
+    takenShotsCoordinatesArray.push([row, col]);
+    return enemy.receiveAttack(row, col);
+  };
+  return { attack, receiveAttack };
+};
+export { ComputerPlayer };
--- Player.js
+++ Player.js
@@ -1,19 +1,14 @@
 const Player = (board, computer) => {
     let playerBoard = board;
-    let hasPlayed = false;
     let enemy = computer;
-
-    const changePlayStatus = () => hasPlayed = !hasPlayed;
 
     const receiveAttack = (row, col) => {
        return playerBoard.receiveAttack(row, col);
     }
-    
+
     const attack = (row, col) => {
-        if(!hasPlayed){
-            changePlayStatus();
-            return enemy.receiveAttack(row, col);
-        }
+        return enemy.receiveAttack(row, col);
+        
     }
     const getBoardOfPlayer = () => playerBoard;
     return {attack, receiveAttack, getBoardOfPlayer};
